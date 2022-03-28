59a7f19029b951a6600c71803dcb3617f3697b6d
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-03-29 00:07:59 +0800

    install jq

--- 233v334.sh
+++ 233v334.sh
@@ -1,10 +1,10 @@
 if [[ -e "/usr/bin/yum" ]] 
 then
   yum update -y
-  yum install -y lrzsz git zip unzip curl wget qrencode libcap
+  yum install -y lrzsz git zip unzip curl wget qrencode libcap jq
 else
   apt-get update -y
-  apt-get install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus
+  apt-get install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus jq
 fi
 
 # 防止处于 /tmp/233boy/v2ray 目录
